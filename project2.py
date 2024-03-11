89438f8164d97cadd86fdb54c61c815a16ce3848
Zahir Mahammad <zahirmd1604@gmail.com>
2024-03-10 22:17:52 -0400

    Final commit

--- project2.py
+++ project2.py
@@ -6,30 +6,27 @@ import time
 # ------------------Obstacle Space Creation-----------------------------------
 
 # Initializing Color
-clearance = (255,255,0)
+# color = (228,155,15)
 color = (255,0,0)
 
 bloat=5
 
-
-
 # Define the center and size of the hexagon
 center_x = 650
 center_y = 250
-size = 150  # Side length of the hexagon
+size = 150          # Side length of the hexagon
 
-# Calculate the vertices of the hexagon
-# Define the center and size of the hexagon
 center_x = 650
-center_y = 250
-size = 150  # Side length of the hexagon
+center_y = 250      # Center of Hexagon
+size = 150          # Side length of the hexagon
 
 # Calculate the vertices of the hexagon with a 90-degree rotation
 num_sides = 6
 angle = 2 * np.pi / num_sides
-rotation_angle = np.pi / 2  # 90 degrees in radians
+rotation_angle = np.pi / 2          # 90 degrees in radians
 vertices = []
 bloat_vertices = []
+# find vertices of the hexagon
 for i in range(num_sides):
     rotated_angle = angle * i + rotation_angle
     cx = center_x + size * np.cos(rotated_angle)
@@ -54,16 +51,15 @@ def line_equation(p1, p2):
 # Calculate the equations of the lines of the hexagon
 line_equations = []
 for i in range(num_sides):
-    p1 = vertices[i]
-    p2 = vertices[(i + 1) % num_sides]  # Wrap around to the first vertex for the last side
+    p1 = bloat_vertices[i]
+    p2 = bloat_vertices[(i + 1) % num_sides]  # Wrap around to the first vertex for the last side
     m, b = line_equation(p1, p2)
     line_equations.append((m, b))
-    # print(f"Side {i+1}: y = {m}x + {b}")
+
 
 def InsideObstacle(location):
-    # print(location)
     x, y = location
-    # hexagon check
+    # hexagon check with bloat
     f1 = y - line_equations[0][0]*x-line_equations[0][1]
     f2 = x - line_equations[1][1]
     f3 = y - line_equations[2][0]*x-line_equations[2][1]
@@ -73,16 +69,16 @@ def InsideObstacle(location):
     if f1<0 and f2>0 and f3>0 and f4>0 and f5<0 and f6<0:
         return True
 
-    # Check walls
+    # Check walls with bloat
     if x<0+bloat or x>1200-bloat or y<0+bloat or y>500-bloat:
         return True
     # First Rectangle Check
     if x>100-bloat and x<175+bloat and y>0-bloat and y<400+bloat:
         return True
-    # Second Rectangle Check
+    # Second Rectangle Check with bloat
     if x>275-bloat and x<350+bloat and y>100-bloat and y<500+bloat:
         return True
-    # Reverse C Shape Check
+    # Reverse C Shape Check with bloat
     if x>900-bloat and x<1020 and y>50-bloat and y<125+bloat:
         return True
     if x>1020-bloat and x<1100+bloat and y>50-bloat and y<450+bloat:
@@ -96,9 +92,6 @@ def InsideObstacle(location):
 
 
 def create_obstacle_space():
-    # env_as_array = pygame.surfarray.array3d(env)
-    # obstacle_space = np.copy(env_as_array)
-    # obstacle_space = obstacle_space[:,:,0]
     obstacle_space = np.zeros((1200, 500))
     for i in range(obstacle_space.shape[0]):
         for j in range(obstacle_space.shape[1]):
@@ -107,21 +100,7 @@ def create_obstacle_space():
 
 # ------------------Building Environment-----------------------------------
 
-
-
-
-
-
-
-# --------- Not in use ------------------
-class Node():
-    def __init__(self, location, parent_loc, cost) -> None:
-        self.location = location
-        self.parent = parent_loc
-        self.c2c = cost
-# --------- Not in use ------------------
-
-
+# --> Perform Up Action given pose and cost
 def Up(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]
@@ -131,6 +110,7 @@ def Up(pose, cost):
     else:
         return None
 
+# --> Perform Down Action given pose and cost
 def Down(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]
@@ -140,6 +120,7 @@ def Down(pose, cost):
     else:
         return None
 
+# --> Perform Left Action given pose and cost
 def Left(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]-1
@@ -149,6 +130,7 @@ def Left(pose, cost):
     else:
         return None
 
+# --> Perform Right Action given pose and cost
 def Right(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]+1
@@ -158,6 +140,7 @@ def Right(pose, cost):
     else:
         return None
 
+# --> Perform TopLeft Action given pose and cost
 def TopLeft(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]-1
@@ -167,6 +150,7 @@ def TopLeft(pose, cost):
     else:
         return None
 
+# --> Perform TopRight Action given pose and cost
 def TopRight(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]+1
@@ -176,6 +160,7 @@ def TopRight(pose, cost):
     else:
         return None
 
+# --> Perform BottomLeft Action given pose and cost
 def BottomLeft(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]-1
@@ -185,6 +170,7 @@ def BottomLeft(pose, cost):
     else:
         return None
 
+# --> Perform BottomRight Action given pose and cost
 def BottomRight(pose, cost):
     if 1200>pose[0]>=0 and 500>pose[1]>=0:
         x = pose[0]+1
@@ -195,28 +181,26 @@ def BottomRight(pose, cost):
         return None
 
 
-
-
-
+# Dijkstra Algorithm
 def dijkstra(start, goal):
-    open_list = []
-    closed_list=[]
-    check_closed = set()
+    open_list = []                      # open list
+    closed_list=[]                      # closed list
+    check_closed = set()                # closed set to check if node in closed list
 
-    open_list.append([0, start, start])
+    open_list.append([0, start, start])    
 
     while True:
         if len(open_list)!=0:
-            new_node = heapq.heappop(open_list)
+            new_node = heapq.heappop(open_list)     # heappop
 
-            closed_list.append(new_node)
-            check_closed.add(new_node[1])
+            closed_list.append(new_node)            # add to closedlist
+            check_closed.add(new_node[1])           # add to check_closed
 
-            if new_node[1] == goal:
-                print("BackTracking..!")
-                return (closed_list, new_node)
+            if new_node[1] == goal:                 # check if its the goal node
+                print("BackTracking..!")            
+                return (closed_list, new_node)      # return closed_list and goal_node
             else:
-                actions =  [Up(new_node[1], new_node[0]), 
+                actions =  [Up(new_node[1], new_node[0]),       # perform all actions and add them to list
                             Down(new_node[1], new_node[0]),
                             Right(new_node[1], new_node[0]),
                             Left(new_node[1], new_node[0]),
@@ -225,75 +209,56 @@ def dijkstra(start, goal):
                             BottomLeft(new_node[1], new_node[0]),
                             BottomRight(new_node[1], new_node[0])] 
                 for action in actions:
-                    if action is not None:
+                    if action is not None:                  # check if action not None
                         x, y = action[1]
-                        # print("y: ", y)
-                        if obstacle_space[x, y] == 0 and action[1] not in check_closed:
-                            # print(action[1])
-                            # print(open_list_array[:,1])
+                        if obstacle_space[x, y] == 0 and action[1] not in check_closed:     # check if state is not in obstacle and closed_list
                             if len(open_list)!=0:
                                 arr = np.array(open_list, dtype=object)
-                                if action[1] in list(arr[:,1]):
-                                    row_index = list(arr[:, 1]).index(action[1])
-                                    if open_list[row_index][0] > action[0]:
-                                        # print("index: ", row_index)
+                                if action[1] in list(arr[:,1]):                             # if state in action list
+                                    row_index = list(arr[:, 1]).index(action[1])        
+                                    if open_list[row_index][0] > action[0]:                 # if current cost is less, update the cost in openlist
                                         open_list[row_index][0] = action[0]
                                         open_list[row_index][2] = new_node[1]
-                                        # heapq.heapify(open_list)
                                     continue
-                            # else:
-                            open_list.append([action[0], action[1], new_node[1]])
-                            # print([action[0], action[1], new_node[1]])
-                            # c+=1
-                            # heapq.heappush(open_list, [action[0], action[1], new_node[1]])
-                # print("ACtions: ", c)
-
+                            open_list.append([action[0], action[1], new_node[1]])          # else, append to the open_list
         else:
             print("No path Found!")
             break
 
-
-
-
-
+# --Backtrack using closed_list
 def backtrack(closed_list, goal_node):
     track = []
-    curr_node = goal_node
-    heapq.heapify(closed_list)
-    while curr_node[1]!=start:
-        for node in closed_list:
-            if curr_node[2] == node[1]:
+    curr_node = goal_node           # set goal_node as current_node
+    # heapq.heapify(closed_list)  
+    while curr_node[1]!=start:              # until current_node is start_node
+        for node in closed_list:        
+            if curr_node[2] == node[1]:     # find the parent_node
                 track.append(node)
                 break
-        curr_node = track[-1]
+        curr_node = track[-1]       # set parent_node as curr_node
 
-    return track[::-1]
-
-
-
-
-
-
-
-
+    return track[::-1]              # reverse the list and return
 
 
 
 # ---Dijkstra Algorithm Implement--------
 obstacle_space = create_obstacle_space()
+print("\n-------------------------------------------")
+print("Dijkstra -> Environment Size: ", obstacle_space.shape)
 print("-------------------------------------------")
-print("Environment Size: ", obstacle_space.shape)
 
 
 r_color = (255,0,255)
 e_color = (0,255,0)
 
+# -- User Input----#
 while True:
+    print("\n##----------Start-Node----------##")
     startx = int(input("Start Point - Enter x: "))
     starty = int(input("Start Point - Enter y: "))
     try:
         if obstacle_space[startx, starty] == -1:
-            print("Start pose is in Obstacle Space!")
+            print("Start pose - in Obstacle Space!")
             print("Please select a pose that is not in Obstacle Space")
         else:
             start = (startx, 500-starty)
@@ -301,11 +266,12 @@ while True:
     except:
         print("Enter values given in the range")
 while True:
+    print("\n##----------Goal-Node----------##")
     endx = int(input("End Point - Enter x: "))
     endy = int(input("End Point - Enter y: "))
     try:
         if obstacle_space[endx, endy] == -1:
-            print("End pose is in Obstacle Space!")
+            print("End pose - in Obstacle Space!")
             print("Please select a pose that is not in Obstacle Space")
         else:
             end = (endx, 500-endy)
@@ -316,6 +282,7 @@ while True:
 print("-------------------------------------------")
 print("Djikstra Loading....")
 
+# ---Calculate time while implementing dijkstra and tracking---
 s_time = time.time()
 closed_list, goal_node = dijkstra(start, end)
 track = backtrack(closed_list, goal_node)
@@ -326,7 +293,6 @@ print("Time taken: ",e_time-s_time," seconds")
 
 
 # ------------------------------Draw Environment---------------------
-
 pygame.init()
 
 # Initializing env
@@ -366,10 +332,7 @@ for i, node in enumerate(track):
     pygame.display.flip()
     pygame.time.Clock().tick(60)
 
-
-
-
-
+# -- Hold the window untill closed--- 
 run = True
 
 
